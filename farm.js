d0f5b0c9438c175e74800da8e9b5ffe06f796ef8
hansolo-123 <99199422+hansolo-123@users.noreply.github.com>
2022-06-22 11:12:43 +0200

    Add files via upload
    first upload tests pass 4/4

---
+++ farm.js
@@ -0,0 +1,18 @@
+const getYieldForPlant = (corn) => {
+  return corn.yield;
+};
+
+const getYieldForCrop = (input) => {
+  return input.numCrops * input.crop.yield;
+};
+
+const getTotalYield = ({ crops }) => {
+  if (crops[0].numCrops === 0) return 0;
+  else sum = [];
+  for (let i of crops) {
+    sum.push(i.numCrops * i.crop.yield);
+  }
+  return sum.reduce((total, arg) => total + arg, 0);
+};
+
+module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield };
